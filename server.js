d309d84f9ab1697f55a1002e5778af7b32d3c100
jiamori <lifesunny719@gmail.com>
2026-06-19 21:15:04 +0800

    修正錯誤

--- server.js
+++ server.js
@@ -97,12 +97,7 @@ const requestListener = (req, res) => {
     res.writeHead(200, headers);
     res.end();
   } else {
-    res.writeHead(404, headers);
-    res.write(JSON.stringify({
-      "status": false,
-      "message": "無此網路路由"
-    }));
-    res.end();
+    errHandle(res);
   }
   
 }
